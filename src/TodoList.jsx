e5c4517866050454f5cc73a6ab6514f6857ff934
Fabricio Guerra <fabricio.guerra@itbeta2.com.br>
2025-05-01 11:01:58 -0300

    feat: implementação do filtro

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-const AddTodo = ( { addTodo }) => {
-
+const AddTodo = ({ addTodo }) => {
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       const input = event.target;
@@ -11,7 +10,7 @@ const AddTodo = ( { addTodo }) => {
         input.value = "";
       }
     }
-  }
+  };
 
   return (
     <input
@@ -22,16 +21,21 @@ const AddTodo = ( { addTodo }) => {
   );
 };
 
-const TodoFilter = () => {
+const TodoFilter = ({ setFilter }) => {
+  const handleFilterChange = (filter) => (event) => {
+    event.preventDefault();
+    setFilter(filter);
+  };
+
   return (
     <div className="center-content">
-      <a href="#" id="filter-all">
+      <a onClick={handleFilterChange("all")} href="#" id="filter-all">
         Todos os itens
       </a>
-      <a href="#" id="filter-done">
+      <a onClick={handleFilterChange("done")} href="#" id="filter-done">
         Concluídos
       </a>
-      <a href="#" id="filter-pending">
+      <a onClick={handleFilterChange("pending")} href="#" id="filter-pending">
         Pendentes
       </a>
     </div>
@@ -39,12 +43,10 @@ const TodoFilter = () => {
 };
 
 const TodoItem = ({ todo, markTodoAsDone }) => {
-  
   const handleClick = () => {
     markTodoAsDone(todo.id);
-  }
+  };
 
-  
   return (
     <>
       {todo.done ? (
@@ -60,12 +62,18 @@ const TodoItem = ({ todo, markTodoAsDone }) => {
 };
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([{id: crypto.randomUUID(), text: "Learn React", done: false }, {id: crypto.randomUUID(), text: "Learn JS", done: true }]);
+  const [todos, setTodos] = useState([
+    { id: crypto.randomUUID(), text: "Learn React", done: false },
+    { id: crypto.randomUUID(), text: "Learn JS", done: true },
+  ]);
+  const [filter, setFilter] = useState("all");
+  const shouldRender = filter === "pending" ? false : true;
+  const viewAll = filter === "all";
 
   const addTodo = (text) => {
     const newTodo = { id: crypto.randomUUID(), text, done: false };
     setTodos((prevTodos) => [...prevTodos, newTodo]);
-  }
+  };
 
   const markTodoAsDone = (id) => {
     setTodos((prevTodos) =>
@@ -73,22 +81,26 @@ const TodoList = () => {
         todo.id === id ? { ...todo, done: true } : todo
       )
     );
-  }
-
+  };
 
   return (
     <>
       <h1>Todo List</h1>
       <div className="center-content">
-        Versão inicial da aplicação de lista de tarefas para a disciplina
-        SPODWE2
+        Versão inicial da aplicação de lista de tarefas para a disciplina SPODWE2
       </div>
-      <TodoFilter />
+      <TodoFilter setFilter={setFilter} />
       <AddTodo addTodo={addTodo} />
       <ul id="todo-list">
-        {todos.map((todo, index) => (
-          <TodoItem key={index} todo={todo} markTodoAsDone={markTodoAsDone} />
-        ))}
+        {viewAll
+          ? todos.map((todo, index) => (
+              <TodoItem key={index} todo={todo} markTodoAsDone={markTodoAsDone} />
+            ))
+          : todos
+              .filter((n) => n.done === shouldRender)
+              .map((todo, index) => (
+                <TodoItem key={index} todo={todo} markTodoAsDone={markTodoAsDone} />
+              ))}
       </ul>
     </>
   );
